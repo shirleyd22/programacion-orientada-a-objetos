d9c4f06444f84134017d834889f558baa70b190a
shirleyd22 <sd.parralesp@uea.edu.ec>
2026-01-10 16:58:04 -0500

    ejemplo de conversion de temperaturas

---
+++ temperature.py
@@ -0,0 +1,19 @@
+# een este programa voy a convertir una temperatura de grados Celsius a Fahrenheit.
+# Utiliza diferentes tipos de datos y sigue las convenciones de nomenclatura en Python.
+
+# Se solicita al usuario que ingrese su nombre
+user_name = input("por favor ingrese su nombre: ")
+
+# Se solicita al usuario que ingrese la temperatura en grados Celsius como un tipo de datos float
+celsius_temperature = float(input("por favor ingrese la temperatura en grados Celsius: "))
+
+# Se realiza la conversión de Celsius a Fahrenheit
+fahrenheit_temperature = (celsius_temperature * 9 / 5) + 32
+
+# Variable booleana que indica si la conversión fue exitosa
+conversion_successful = True
+
+# Se muestran los resultados en pantalla
+print("Hola,", user_name)
+print("La temperatura en grados Fahrenheit es:", fahrenheit_temperature)
+print("¿La conversión fue exitosa?", conversion_successful)
